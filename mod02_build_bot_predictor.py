95782abdf72f4af80a761e0d0e812f0678652b13
Noah Steinberg <noah.steinberg@uconn.edu>
2026-02-09 16:01:21 -0500

    updated mod02 assignment

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -19,28 +19,3 @@ def train_model(X, y, seed=seed):
     )
     model.fit(X, y)
     return model
-
-
-
-# # packages
-# import pandas as pd
-# from sklearn.ensemble import GradientBoostingClassifier
-
-# # set seed
-# seed = 314
-
-# def train_model(X, y, seed=seed):
-#     """
-#     Build a GBM on given data
-#     """
-#     model = GradientBoostingClassifier(
-#         learning_rate=0.05,
-#         n_estimators=400,
-#         subsample=0.8,
-#         min_samples_leaf=5,
-#         min_samples_split=10,
-#         max_features="sqrt",
-#         random_state=seed
-#     )
-#     model.fit(X, y)
-#     return model
